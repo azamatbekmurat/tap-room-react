b75512f42bb659523f05daced03997b16511ac49
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-29 12:45:03 -0700

    add components and assets folders

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        
+      </Switch>
+    </div>
+  )
+}
+
+export default App
