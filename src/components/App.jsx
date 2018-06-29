9b3917e04c3d00ac6dbc1d6a5e8e585dbdb72e57
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-29 15:37:06 -0700

    add routes to app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 import { Switch, Route } from 'react-router-dom'
+import KegList from './KegList'
+import NewKegForm from './NewKegForm'
+import Error404 from './Error404'
 
 function App(){
   return (
     <div>
       <Header/>
       <Switch>
-        
+        <Route exact path='/' component={KegList} />
+        <Route path='/newkegform' component={NewKegForm} />
+        <Route component={Error404} />
       </Switch>
     </div>
   )
