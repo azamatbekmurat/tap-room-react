64c6fd2018eae089f3318d8d45a90a8ac7b1b99f
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-29 17:46:02 -0700

    add image component

---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import newBelg from '../assets/images/new_belg.png'
+
+function Image(){
+  return (
+    <div>
+      <img src={newBelg} alt="beer" width="200" height="150" />
+    </div>
+  )
+}
+
+export default Image
