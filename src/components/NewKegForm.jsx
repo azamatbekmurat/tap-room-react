ffa6af87c9744a834b04f31a75413b7c79f397b5
Guest User <Guest@epicodus-50.local>
2018-07-06 15:00:03 -0700

    update KegList and NewKegForm components

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { v4 } from 'uuid'
+import Moment from 'moment'
 
-function NewKegForm() {
+function NewKegForm(props) {
+  let _name = null
+  let _brewer = null
+  let _description = null
+  let _abv = null
+  let _price = null
+  let _remaining = null
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefauld();
+    props.onNewKegCreation({name: _name.value, brewer: _brewer.value, description: _description.value, abv: _abv.value, price: _price.value, remaining: _remaining.value});
+    _name.value = '';
+    _brewer.value = '';
+    _description.value = '';
+    _abv.value = '';
+    _price.value = '';
+    _remaining.value = '';
+  }
+
   var inputStyle =  {
     padding: '10px',
     fontSize: '20px',
@@ -19,40 +40,50 @@ function NewKegForm() {
   }
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewKegFormSubmission}>
         <input
           style={inputStyle}
           type='text'
           id='name'
-          placeholder='Beer Name'/>
+          placeholder='Beer Name'
+          ref={(input) => {_name = input}}/>
         <input
           style={inputStyle}
           type='text'
           id='brewer'
-          placeholder='Brewer Name'/>
+          placeholder='Brewer Name'
+          ref={(input) => {_brewer = input}}/>
         <input
           style={inputStyle}
           type='text'
           id='abv'
-          placeholder='Alcohol by volume'/>
+          placeholder='Alcohol by volume'
+          ref={(input) => {_abv = input}}/>
         <input
           style={inputStyle}
           type='text'
           id='price'
-          placeholder='Price'/>
+          placeholder='Price'
+          ref={(input) => {_price = input}}/>
         <input
           style={inputStyle}
           type='text'
           id='remaining'
-          placeholder='Volume'/>
+          placeholder='Volume'
+          ref={(input) => {_remaining = input}}/>
         <textarea
           style={inputStyle}
           id='description'
-          placeholder='Give brief description of a beer'/>
+          placeholder='Give brief description of a beer'
+          ref={(input) => {_description = input}}/>
         <button style={button} type='submit'>Add new keg!</button>
       </form>
     </div>
   )
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
 }
 
 export default NewKegForm
